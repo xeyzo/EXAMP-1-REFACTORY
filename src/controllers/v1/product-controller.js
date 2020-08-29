ea2396056a1d9a4ba39f15398858499fd268a319
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-29 12:44:58 +0700

    update product function

--- src/controllers/v1/product-controller.js
+++ src/controllers/v1/product-controller.js
@@ -21,7 +21,18 @@ class ProductController {
     }
 
     static async update (req, res) {
-
+        // req.body.data.userId = 
+        const { name, stock, price } = req.body.data
+        try {
+            const product = await Product.findByPk(req.params.id)
+            product.name = name
+            product.stock = stock
+            product.price = price
+            await product.save()
+            res.status(200).json(response('success', 'product updated', product))
+        } catch (err) {
+            res.status(500).json(response('fail', err.message))
+        }
     }
 
     static async destroy (req, res) {
