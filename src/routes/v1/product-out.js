2a1bbe40e0c06c89c4a29ec2cb4e684cf3dcd543
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-29 14:05:14 +0700

    product-out crud complete

--- src/routes/v1/product-out.js
+++ src/routes/v1/product-out.js
@@ -4,4 +4,8 @@ const ProductOutController = require('../../controllers/v1/product-out-controlle
 router.get('/', ProductOutController.getAll)
     .post('/', ProductOutController.create)
 
+router.put('/:id', ProductOutController.update)
+    .delete('/:id', ProductOutController.destroy)
+    .get('/:id', ProductOutController.getById)
+
 module.exports = router
