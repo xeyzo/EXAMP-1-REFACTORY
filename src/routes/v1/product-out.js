d796fa379e9dca34dc6aef66931366210d8f3d64
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-29 13:51:41 +0700

    add post method to out endpoint

--- src/routes/v1/product-out.js
+++ src/routes/v1/product-out.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const ProductOutController = require('../../controllers/v1/product-out-controller')
 
 router.get('/', ProductOutController.getAll)
+    .post('/', ProductOutController.create)
 
 module.exports = router
