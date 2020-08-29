162c04377fcb2692c233ac9caea4f1eea3e9012f
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-29 12:45:55 +0700

    define another product route

--- src/routes/v1/products.js
+++ src/routes/v1/products.js
@@ -2,5 +2,10 @@ const router = require('express').Router()
 const ProductController = require('../../controllers/v1/product-controller')
 
 router.get('/', ProductController.getAll)
+    .post('/', ProductController.create)
+
+router.put('/:id', ProductController.update)
+    .delete('/:id', ProductController.destroy)
+    .get('/:id', ProductController.getById)
 
 module.exports = router
