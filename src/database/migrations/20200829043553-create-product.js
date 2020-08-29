1a7befbf0d6fdae41004185935f4969ca2cb0497
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-29 11:58:22 +0700

    generate model and migration for product table

---
+++ src/database/migrations/20200829043553-create-product.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      stock: {
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      price: {
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
