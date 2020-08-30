41953bfacc2c9e7dcb6b5bba18fda1bb9745228c
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-30 20:09:41 +0700

    report controller

--- src/controllers/v1/report-controller.js
+++ src/controllers/v1/report-controller.js
@@ -1,56 +1,48 @@
 const { In, ProductOut, Product } = require('../../database/models')
 const reportQueue = require('../../queue/report')
+const generatePDF = require('../../services/generate-pdf')
 const response = require('../../helpers/response')
 
 class ReportController {
     static async print (req, res) {
         const { type } = req.params
-        if (type.toLowerCase() === 'in') {
-            try {
-                const productIn = await In.findAll({
-                    include: [
-                        { model: Product, as: 'product' }
-                    ]
-                })
-
-                const productData = productIn.map(p => p.dataValues).map(p => { p['product'] = p.product.dataValues; return p})
-
+        try {
+            const productIn = await In.findAll({
+                include: [
+                    { model: Product, as: 'product' }
+                ]
+            })
+             
+            const productOut = await ProductOut.findAll({
+                include: [
+                    { model: Product, as: 'product' }
+                ]
+            })
+            if (type === 'in') {
                 reportQueue.add({
-                    pdf: productData,
-                    user: req.user.email,
+                    pdf: productIn.map(p => p.dataValues).map(p => { p['product'] = p.product.dataValues; return p}),
+                    user: req.user.dataValues.email,
                 }, {
                     delay: 1000,
                     repeat: {
                         cron: '0 01 0 28-31 * *',
                     }
                 })
-                res.status(200).json(response('success', 'monthly report sent to ur email'))
-            } catch (err) {
-                res.status(500).json(response('fail', err.message))
-            }
-        } else {
-            try {
-                const productIn = await In.findAll({
-                    include: [
-                        { model: Product, as: 'product' }
-                    ]
-                })
-
-                const productData = productIn.map(p => p.dataValues).map(p => { p['product'] = p.product.dataValues; return p})
-
+            } else if (type === 'out') {
                 reportQueue.add({
-                    pdf: productData,
-                    user: req.user.email,
+                    pdf: productOut.map(p => p.dataValues).map(p => { p['product'] = p.product.dataValues; return p}),
+                    user: req.user.dataValues.email,
                 }, {
                     delay: 1000,
                     repeat: {
                         cron: '0 01 0 28-31 * *',
                     }
                 })
-                res.status(200).json(response('success', 'monthly report sent to ur email'))
-            } catch (err) {
-                res.status(500).json(response('fail', err.message))   
             }
+            res.status(200).json(response('success', 'monthly report sent to ur email'))
+
+        } catch (err) {
+            res.status(500).json(response('fail', err.message))
         }
     }
 }
